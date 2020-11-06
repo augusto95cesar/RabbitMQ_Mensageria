e0fea394525e8ef6a3edce4071ab5a4e50506720
Augusto Cesar <augusto95cesar@gmail.com>
2020-11-06 19:27:30 -0300

    Projeto de Estudo RabbitMQ

---
+++ RabbitMQ.Get/RabbitMQ.Get/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Get
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionFactory connFactory = new ConnectionFactory();
+            connFactory.Uri = new Uri("amqp://localhost");
+            using (var conn = connFactory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                // channel.QueueDeclare("queue1", false, false, false, null);
+                // var queueName = "queue1"; 
+                var queueName = "hello";
+                var data = channel.BasicGet(queueName, false);
+                if (data == null)
+                    Console.WriteLine(".............................................");
+                else
+                {
+                   //onsole.WriteLine(data.Body);
+
+                    var body = data.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    channel.BasicAck(data.DeliveryTag, false);
+
+                  //Console.WriteLine(".............................................");
+                    Console.WriteLine(message);
+                }
+               //onsole.WriteLine("...................FIM SYSTEM.......................");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Publish/RabbitMQ.Publish/Program.cs
@@ -0,0 +1,64 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RabbitMQ.Publish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Funciona........................................................................................
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            //Console.WriteLine(" Press [enter] to exit.");
+            //Console.ReadLine();
+
+            //Funciona........................................................................................
+            //ConnectionFactory connFactory = new ConnectionFactory();
+            //connFactory.Uri = new Uri("amqp://localhost");
+
+            //    using (var conn = connFactory.CreateConnection())
+            //    using (var channel = conn.CreateModel())
+            //    {
+            //        // The message we want to put on the queue
+            //        var message = DateTime.Now.ToString("F");
+            //        // the data put on the queue must be a byte array
+            //        var data = Encoding.UTF8.GetBytes(message);
+            //        // ensure that the queue exists before we publish to it
+            //        var queueName = "queue1";
+            //        bool durable = true;
+            //        bool exclusive = false;
+            //        bool autoDelete = false;
+            //        channel.QueueDeclare(queueName, durable, exclusive, autoDelete, null);
+            //        // publish to the "default exchange", with the queue name as the routing key
+            //        var exchangeName = "";
+            //        var routingKey = "queue1";
+            //        channel.BasicPublish(exchangeName, routingKey, null, data);
+            //    }
+            //    // return new EmptyResult();
+        }
+    }
+}
